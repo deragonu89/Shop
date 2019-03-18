15bd739ae9fb7464d1e1411872298fb9a99d5535
Jorge Navarro <deragonu89@live.com>
2019-03-18 01:12:47 -0600

    Change Password. Change User.

--- Shop.Web/Helpers/IUserHelper.cs
+++ Shop.Web/Helpers/IUserHelper.cs
@@ -15,5 +15,9 @@
 
         Task LogoutAsync();
 
+        Task<IdentityResult> UpdateUserAsync(User user);
+
+        Task<IdentityResult> ChangePasswordAsync(User user, string oldPassword, string newPassword);
+
     }
 }
